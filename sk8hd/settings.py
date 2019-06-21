3b45f00f706220905a2176afa6174c1903eb6f11
Plock Party <plock@posteo.net>
2019-06-21 11:02:33 +0200

    add logginf

--- sk8hd/settings.py
+++ sk8hd/settings.py
@@ -129,21 +129,67 @@ TEMPLATES = [
 
 if DEBUG is True: 
     LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+            }
         },
+        'loggers': {
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            },
+        }
     }
-}
+else: 
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt' : "%d/%b/%Y %H:%M:%S"
+            },
+        },
+        'handlers': {
+            'null': {
+                'level':'DEBUG',
+                'class':'django.utils.log.NullHandler',
+            },
+            'logfile': {
+                'level':'DEBUG',
+                'class':'logging.handlers.RotatingFileHandler',
+                'filename': SITE_ROOT + "/logfile",
+                'maxBytes': 50000,
+                'backupCount': 2,
+                'formatter': 'standard',
+            },
+            'console':{
+                'level':'INFO',
+                'class':'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers':['console', 'logfile'],
+                'propagate': True,
+                'level':'WARN',
+            },
+            'django.db.backends': {
+                'handlers': ['console', 'logfile'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+            '': {
+                'handlers': ['console', 'logfile'],
+                'level': 'DEBUG',
+            },
+        }
+    }
 
 WSGI_APPLICATION = 'sk8hd.wsgi.application'
 
